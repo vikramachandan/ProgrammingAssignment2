b8752b55eeb97f3364b9fa844d340008cc4fddec
Vikramachandran Selvakumar <row4vikram@gmail.com>
2015-12-27 17:03:39 -0500

    Coursera R Programming Language week 2 Assignment
    Coursera R Programming Language week 2 Assignment - Updated more comments to make things very clear.

--- cachematrix.R
+++ cachematrix.R
@@ -3,12 +3,16 @@
 ## from CoursEra team as part of R Programming Language Course.
 ## Took this course Dec 2015 and as part of assignment students participating were expected to 
 ## complete the functions as part of week 2 programming assignment.
-## makeCacheMatrix function takes special object/vetor(matrix) as input and returns special vector/object list of functions.
+
+## makeCacheMatrix function takes matrix object/vetor as input and returns special vector/object, list of functions.
+## precondition is input matrix must be a square matrix on which inverse could be calculate.
 ## set function initializes the matrix object and inverse mat objects.
 ## get function returns the matrix
 ## compinv function caches the inverse matrix into invmat
 ## getinv function returns invmat
-
+## Example invocation
+## t <- matrix(1:4,2,2)
+## d <- makeCacheMatrix(t)
 
 makeCacheMatrix <- function(x = matrix()) {
 invmat = NULL
@@ -23,10 +27,16 @@ invmat = NULL
 
 }
 
-## cacheSolve function takes object/specical vector returned by makeCachematrix 
-## if the inverse of the matrix is present in the cache it does not compute the inverse of the matrix pointed to by passed vector
-##/object.
+## cacheSolve function takes object/specical vector returned by makeCachematrix as input
+## if the inverse of the matrix is present in the cache it does not compute the inverse of the matrix - returns from the cache
 ## else it computes the inverse of the matrix and returns it.
+##example invocation
+## t <- matrix(1:4,2,2)
+## d <- makeCacheMatrix(t)
+##cacheSolve(d)
+## when you invoke first time you SHOULD NOT see "getting cached data" message on the console
+## when you invoke second time you SHOULD see "getting cached data" message on the console indicating that the inverse is being 
+## picked up from cache rather than getting computed.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
